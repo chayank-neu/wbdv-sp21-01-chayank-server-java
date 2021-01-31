c4858c2b35c688514c7eff047278d81acfd871cf
Chayank11 <chayank.tm@gmail.com>
2021-01-31 13:29:46 -0500

    Created login web page

---
+++ src/main/java/com/example/wbdvsp2101chayankserverjava/WbdvSp2101ChayankServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2101chayankserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbdvSp2101ChayankServerJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WbdvSp2101ChayankServerJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wbdvsp2101chayankserverjava/WbdvSp2101ChayankServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2101chayankserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbdvSp2101ChayankServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
